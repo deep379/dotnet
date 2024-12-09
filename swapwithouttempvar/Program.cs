4c9ed072fd7684ce94f7fb72d48a261dbe1df5c6
Sudip Regmi <sudipregmi379@gmail.com>
2024-12-09 09:10:01 +0545

    swap var code add

---
+++ swapwithouttempvar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapWithouTempVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 6;
+            Console.WriteLine($"before swapping a = {a} and b = {b}");
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"after swapping a = {a} and b = {b}");
+            Console.ReadKey();
+        }
+    }
+}
